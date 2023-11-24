cc331e5301be578e7c3c4c9b4e7ddf68c2d1da87
Gbaole <giabao23479@gmail.com>
2023-11-24 11:44:20 +0700

    reInit

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideNzIcons } from './icons-provider';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -12,5 +14,5 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideNzIcons(), provideNzI18n(en_US), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
 };
